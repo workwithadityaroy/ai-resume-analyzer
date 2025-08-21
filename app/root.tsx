9bf25899fcb629e2fad34f5f1db3b9f25feb4f0f
Aditya Roy <aditya.roy.tpr@pwc.com>
2025-08-22 01:09:15 +0530

    Implemented Authentication with Puter.js

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -24,6 +26,12 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { init } = usePuterStore();
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
   return (
     <html lang="en">
       <head>
@@ -32,6 +40,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
+      <script src="https://js.puter.com/v2/"></script>
       <body>
         {children}
         <ScrollRestoration />
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,12 +2,24 @@ import type { Route } from ".react-router/types/app/routes/+types/home.ts";
 import ResumeCard from "~/components/ResumeCard";
 import Navbar from "~/components/Navbar";
 import { resumes } from "~/../constants";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({ }: Route.MetaArgs) {
   return [{ title: "Resumind" }, { name: "description", content: "Smart feedback for your dream job!" }];
 }
 
 export default function Home() {
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        if (!auth.isAuthenticated) {
+            navigate('/auth?next=/');
+        }
+    }, [auth.isAuthenticated]);
+
   return (
     <main className='bg-[url("/images/bg-main.svg")] bg-cover'>
       <Navbar />
